2f7251438ece2f37a3a18ca25fe4547f17ba4ff3
Aayush Timalsina <aayushtimalsina2@gmail.com>
2024-12-09 21:40:57 +0545

    Implemented the create handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -3,8 +3,6 @@ using Application.Activities;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
@@ -20,6 +18,14 @@ namespace API.Controllers
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
             return await Mediator.Send(new Details.Query{Id = id});
+        }
+ 
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity([FromBody]Activity     activity)
+        {
+            await Mediator.Send(new Create.Command { Activity = activity });
+
+            return Ok();
         }
     }
 }
