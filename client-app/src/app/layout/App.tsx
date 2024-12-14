9e81499f85e9efaee13a5c750152594b801179c8
Aayush Timalsina <aayushtimalsina2@gmail.com>
2024-12-14 11:21:27 +0545

    Implement some CSS and semantic ui to make our Navbar look nice and attractive

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import axios from "axios";
-import { List } from "semantic-ui-react";
+import { Container, List } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
 
@@ -18,14 +18,16 @@ function App() {
   }, []);
 
   return (
-    <div>
+    <>
       <NavBar />
-      <List>
-        {activities.map((activity) => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
+      <Container style={{ marginTop: "7em" }}>
+        <List>
+          {activities.map((activity) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </Container>
+    </>
   );
 }
 
