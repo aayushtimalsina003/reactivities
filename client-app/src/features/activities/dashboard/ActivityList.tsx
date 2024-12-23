74d276ec2495f794ddc517c9a1c3688c022181c3
Aayush Timalsina <aayushtimalsina2@gmail.com>
2024-12-23 18:36:40 +0545

    Updated the activities lists to be displayed in group if it is of same date using the GropedActivities function

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,22 +1,12 @@
-import { Button, Item, Label, Segment } from "semantic-ui-react";
-import { SyntheticEvent, useState } from "react";
+import { Header, Item, Segment } from "semantic-ui-react";
 import { useStore } from "../../../stores/store";
 import { observer } from "mobx-react-lite";
-import { Link } from "react-router-dom";
+import ActivityListItem from "./ActivityListItem";
+import { Fragment } from "react/jsx-runtime";
 
 export default observer(function ActivityList() {
   const { activityStore } = useStore();
-  const { deleteActivity, activitiesByDate, loading } = activityStore;
-
-  const [target, setTarget] = useState("");
-
-  function handleActivityDelete(
-    e: SyntheticEvent<HTMLButtonElement>,
-    id: string
-  ) {
-    setTarget(e.currentTarget.name);
-    deleteActivity(id);
-  }
+  const { groupedActivities } = activityStore;
 
   // Debugging to check IDs
   // console.log(
@@ -25,41 +15,25 @@ export default observer(function ActivityList() {
   // );
 
   return (
-    <Segment>
-      <Item.Group divided>
-        {activitiesByDate.map((activity) => (
-          <Item key={activity.id}>
-            <Item.Content>
-              <Item.Header as="a">{activity.title}</Item.Header>
-              <Item.Meta>{activity.date}</Item.Meta>
-              <Item.Description>
-                <div>{activity.description}</div>
-                <div>
-                  {activity.city}, {activity.venue}
-                </div>
-              </Item.Description>
-              <Item.Extra>
-                <Button
-                  as={Link}
-                  to={`/activities/${activity.id}`}
-                  floated="right"
-                  content="view"
-                  color="blue"
+    <>
+      {groupedActivities.map(([group, activities]) => (
+        <Fragment key={group}>
+          <Header sub color="teal">
+            {group}
+          </Header>
+          <Segment>
+            <Item.Group divided>
+              {activities.map((activity) => (
+                <ActivityListItem
+                  key={activity.id}
+                  activity={activity}
+                  loading={false}
                 />
-                <Button
-                  name={activity.id}
-                  loading={loading && target === activity.id}
-                  onClick={(e) => handleActivityDelete(e, activity.id)}
-                  floated="right"
-                  content="delete"
-                  color="red"
-                />
-                <Label basic content={activity.category} />
-              </Item.Extra>
-            </Item.Content>
-          </Item>
-        ))}
-      </Item.Group>
-    </Segment>
+              ))}
+            </Item.Group>
+          </Segment>
+        </Fragment>
+      ))}
+    </>
   );
 });
