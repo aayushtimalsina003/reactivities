f8f17300f238b190d7cd5cb6a9877c008671d5f3
Aayush Timalsina <aayushtimalsina2@gmail.com>
2024-12-24 15:02:47 +0545

    Enhanced the User Interface and added a style for the HomePage Component

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
-import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
+import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
-import { useStore } from "../../../stores/store";
-import { SyntheticEvent, useState } from "react";
 
 interface Props {
   activity: Activity;
@@ -10,18 +8,6 @@ interface Props {
 }
 
 export default function ActivityListItem({ activity }: Props) {
-  const { activityStore } = useStore();
-  const { deleteActivity, loading } = activityStore;
-
-  const [target, setTarget] = useState("");
-
-  function handleActivityDelete(
-    e: SyntheticEvent<HTMLButtonElement>,
-    id: string
-  ) {
-    setTarget(e.currentTarget.name);
-    deleteActivity(id);
-  }
   return (
     <Segment.Group>
       <Segment>
