010c0cbb5c7702ee13752ef78f9c0d145b97f4c6
Aayush Timalsina <aayushtimalsina2@gmail.com>
2024-12-26 12:56:34 +0545

    Implemented the error handling using an interceptors, as well as added the not found component

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,44 +1,76 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import { Activity } from "../models/activity";
+import { toast } from "react-toastify";
+import { router } from "../routes/Routes";
 
 const sleep = (delay: number) => {
-    return new Promise((reslove) => {
-        setTimeout(reslove, delay);
-    })
-}
+  return new Promise((reslove) => {
+    setTimeout(reslove, delay);
+  });
+};
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = "http://localhost:5000/api";
 
-axios.interceptors.response.use(async response => {
-    try {
-        await sleep(1000);
-        return (response);
-    } catch (error) {
-        console.log(error);
-        return await Promise.reject(error);
-    } 
-})
+axios.interceptors.response.use(
+  async (response) => {
+    await sleep(1000);
+    return response;
+  },
+  (error: AxiosError) => {
+    const { data, status } = error.response as AxiosResponse;
+    switch (status) {
+      case 400:
+        if (data.errors) {
+          const modelStateErrors = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modelStateErrors.push(data.errors[key]);
+            }
+          }
+          throw modelStateErrors.flat();
+        } else {
+          toast.error(data);
+        }
+        break;
+      case 401:
+        toast.error("unauthorized");
+        break;
+      case 403:
+        toast.error("forbidden");
+        break;
+      case 404:
+        router.navigate("/not-found");
+        break;
+      case 500:
+        toast.error("server error");
+        break;
+    }
+    return Promise.reject(error);
+  }
+);
 
-const responseBody = <T> (response: AxiosResponse<T>) => response.data;
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
-    get:<T> (url: string) => axios.get<T>(url).then(responseBody),
-    post:<T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
-    put:<T> (url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
-    delete:<T> (url: string) => axios.delete<T>(url).then(responseBody),
-}
+  get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+  post: <T>(url: string, body: {}) =>
+    axios.post<T>(url, body).then(responseBody),
+  put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+  delete: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+};
 
 const Activities = {
-    list: () => requests.get<Activity[]>('/activities'),
-    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity) => requests.post<void>('/activities', activity),
-    update: (activity: Activity) => requests.put<void>(`/activities/${activity.id}`, activity),
-    delete: (id: string) => requests.delete<void>(`/activities/${id}`)
-}
+  list: () => requests.get<Activity[]>("/activities"),
+  details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+  create: (activity: Activity) => requests.post<void>("/activities", activity),
+  update: (activity: Activity) =>
+    requests.put<void>(`/activities/${activity.id}`, activity),
+  delete: (id: string) => requests.delete<void>(`/activities/${id}`),
+};
 
 const agent = {
-    Activities
-}
+  Activities,
+};
 
-export default agent
+export default agent;
