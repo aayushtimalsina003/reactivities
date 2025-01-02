7e8bd38230c6c2cbbdb31dde80c748a8d56eb6c1
Aayush Timalsina <aayushtimalsina2@gmail.com>
2025-01-02 17:28:58 +0545

    Updated the HomePage and NavBar when the user is logged in or not

--- client-app/src/stores/userStore.ts
+++ client-app/src/stores/userStore.ts
@@ -1,6 +1,8 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import { User, UserFormValues } from "../app/models/user";
 import agent from "../app/api/agent";
+import { store } from "./store";
+import { router } from "../app/routes/Routes";
 
 export default class UserStore {
   user: User | null = null;
@@ -9,12 +11,21 @@ export default class UserStore {
     makeAutoObservable(this);
   }
 
-  getIsLoggedIn() {
+  isLoggedIn() {
     return !!this.user;
   }
 
   login = async (creds: UserFormValues) => {
     const user = await agent.Account.login(creds);
-    console.log(user);
-  }
+    store.commonStore.setToken(user.token);
+    runInAction(() => (this.user = user));
+    router.navigate("/activities");
+  };
+
+  logout = () => {
+    store.commonStore.setToken(null);
+    localStorage.removeItem("jwt");
+    this.user = null;
+    router.navigate("/");
+  };
 }
